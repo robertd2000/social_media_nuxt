bd6c20c28095584930acdaddf09b62d993a1a440
Rob <rob@example.com>
2022-11-04 18:37:46 +0300

    changes db connection

---
+++ server/api/auth/login.post.js
@@ -0,0 +1,60 @@
+import { sendError } from 'h3'
+import { getUserByUsername } from '~~/server/db/users'
+import bcrypt from 'bcrypt'
+import { generateTokens, sendRefreshToken } from '~~/server/utils/jwt'
+import { userTransformer } from '~~/server/transformers/user'
+import { createRefreshToken } from '~~/server/db/refreshTokens'
+
+export default defineEventHandler(async (event) => {
+  const body = await useBody(event)
+
+  const { username, password } = body
+
+  if (!username || !password) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: 'Ivalid params',
+      })
+    )
+  }
+
+  const user = await getUserByUsername(username)
+
+  if (!user) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: 'Username or password is invalid',
+      })
+    )
+  }
+
+  const doesThePasswordMatch = await bcrypt.compare(password, user.password)
+
+  if (!doesThePasswordMatch) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: 'Username or password is invalid',
+      })
+    )
+  }
+
+  const { accessToken, refreshToken } = generateTokens(user)
+
+  await createRefreshToken({
+    token: refreshToken,
+    userId: user.id,
+  })
+
+  sendRefreshToken(event, refreshToken)
+
+  return {
+    access_token: accessToken,
+    user: userTransformer(user),
+  }
+})
---
+++ server/db/users.js
@@ -0,0 +1,29 @@
+import { prisma } from '.'
+import bcrypt from 'bcrypt'
+
+export const createUser = (userData) => {
+  const data = {
+    ...userData,
+    password: bcrypt.hashSync(userData.password, 10),
+  }
+
+  return prisma.user.create({
+    data,
+  })
+}
+
+export const getUserByUsername = (username) => {
+  return prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+}
+
+export const getUserById = (userId) => {
+  return prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  })
+}
